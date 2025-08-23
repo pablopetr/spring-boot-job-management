22246b9d29a8f79ce25d486e977b1e6c21c18f97
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-08-23 19:39:27 -0300

    First commit

---
+++ src/main/java/br/com/pablopetr/spring_boot_job_management/SpringBootJobManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.pablopetr.spring_boot_job_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJobManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJobManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pablopetr/spring_boot_job_management/SpringBootJobManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pablopetr.spring_boot_job_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
