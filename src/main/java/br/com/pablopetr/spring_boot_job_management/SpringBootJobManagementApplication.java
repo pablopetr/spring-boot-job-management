388ac966d5cd9e9d2544cd5c9a52b097e4779b56
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-08-24 12:58:20 -0300

    Add search by email and username to CandidateRepository

--- src/main/java/br/com/pablopetr/spring_boot_job_management/SpringBootJobManagementApplication.java
+++ src/main/java/br/com/pablopetr/spring_boot_job_management/SpringBootJobManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJobManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJobManagementApplication.class, args);
 	}
-
 }
--- src/main/java/br/com/pablopetr/spring_boot_job_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/pablopetr/spring_boot_job_management/modules/candidate/controllers/CandidateController.java
@@ -1,19 +1,28 @@
 package br.com.pablopetr.spring_boot_job_management.modules.candidate.controllers;
 
+import br.com.pablopetr.spring_boot_job_management.exceptions.UserAlreadyExists;
 import br.com.pablopetr.spring_boot_job_management.modules.candidate.CandidateEntity;
 import br.com.pablopetr.spring_boot_job_management.modules.candidate.CandidateRepository;
+import br.com.pablopetr.spring_boot_job_management.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class CandidateController {
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/candidate")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        return this.candidateRepository.save(candidateEntity);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
