736f989e56cb08c52acf37acfda11df989a2087c
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-08-24 14:29:15 -0300

    Create JobRepository

---
+++ src/main/java/br/com/pablopetr/spring_boot_job_management/modules/company/repositories/JobRepository.java
@@ -0,0 +1,9 @@
+package br.com.pablopetr.spring_boot_job_management.modules.company.repositories;
+
+import br.com.pablopetr.spring_boot_job_management.modules.company.entities.JobEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+}
