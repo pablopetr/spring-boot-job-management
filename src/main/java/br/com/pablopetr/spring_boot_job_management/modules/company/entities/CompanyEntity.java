a8ba1296e882cbdce5cc41f2fc77f7878b4201c3
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-08-24 13:02:27 -0300

    Create CompanyEntity

---
+++ src/main/java/br/com/pablopetr/spring_boot_job_management/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,44 @@
+package br.com.pablopetr.spring_boot_job_management.modules.company.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "companies")
+@Data
+public class CompanyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Pattern(regexp = "^\\S+$", message = "Username must not contain spaces")
+    private String username;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Email(message = "Email should be valid")
+    private String password;
+
+    @NotBlank
+    @URL(message = "Website should be a valid URL")
+    private String website;
+
+    @NotBlank
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
