12af45e773d3387fee7fcf21c0e573bd4868a839
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-08-25 21:32:30 -0300

    Encode candidate password in CreateCandidateUseCase

--- src/main/java/br/com/pablopetr/spring_boot_job_management/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/pablopetr/spring_boot_job_management/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,12 +4,16 @@ import br.com.pablopetr.spring_boot_job_management.exceptions.UserAlreadyExists;
 import br.com.pablopetr.spring_boot_job_management.modules.candidate.CandidateEntity;
 import br.com.pablopetr.spring_boot_job_management.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         if(
@@ -18,6 +22,10 @@ public class CreateCandidateUseCase {
         ) {
             throw new UserAlreadyExists();
         }
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+
+        candidateEntity.setPassword(password);
 
         return candidateRepository.save(candidateEntity);
     }
