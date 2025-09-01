b93a6c9e4407a4101e963707eba0e337e214258b
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-09-01 17:09:41 -0300

    Install grafana

--- src/main/java/br/com/pablopetr/spring_boot_job_management/security/SecurityConfig.java
+++ src/main/java/br/com/pablopetr/spring_boot_job_management/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-resources/**",
             "/error",
-            "/actuator/**"
+            "/actuator/**",
     };
 
     @Bean
